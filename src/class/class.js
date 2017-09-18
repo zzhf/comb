20fe4d4937e001012b1159ad79b57124b3a09f1f
zzhf <zzzhf00@sina.com>
2017-09-18 23:57:39 +0800

    新增leaflet构造函数项目注释解析

--- src/class/class.js
+++ src/class/class.js
@@ -34,27 +34,30 @@
 
     L.Class.extend = function(props) {
 
+        //用于返回的新的继承函数
         var NewClass = function() {
 
+            //调用子类初始化构造方法
             if (this.initialize) {
                 this.initialize.apply(this, arguments);
             }
-
+            //钩子函数---调用父类及其父类的构造方法 
             if (this._initHooks) {
                 this.callInitHooks();
             }
         };
 
-        //寄生组合式继承
+        //寄生组合式继承-创建无构造函数体的类
         var F = function() {};
+        //引用父类的原型 --- 此时只是引用关系，如果改变F的prototype亦会修改父类的prototype
         F.prototype = this.prototype;
-
+        //实例化F类
         var proto = new F();
-        //显式指定构造函数
+        //显式指定构造函数,防止instanceof运算符被破坏
         proto.constructor = NewClass;
-
+        //将原型赋值给NewClass,
         NewClass.prototype = proto;
-        //复制L.Class基类方法复制至NewClass
+        //复制L.Class基类方法复制至NewClass, 如L.Class.include、L.Class.mergeOptions等
         for (var item in this) {
             if (this.hasOwnProperty(item)) {
                 NewClass[i] = this[i];
@@ -63,11 +66,14 @@
         //新增静态属性
         if (props.statics) {
             L.extend(NewClass, props.statics);
+            //删除props.statics属性,防止后面L.extend(proto, props)重复新增
             delete props.statics;
         }
 
         if (props.includes) {
-            L.Util.extend.apply(null, [proto].concat(props.includes)); //need to be tested;
+            //扩充子类的方法，在这里主要是为子类新增Events方法
+            L.Util.extend.apply(null, [proto].concat(props.includes)); 
+            //删除props.statics属性,防止后面L.extend(proto, props)重复新增
             delete props.includes;
         }
         //扩充options
@@ -77,12 +83,13 @@
 
         //扩充子类静态属性
         L.extend(proto, props);
-
+        //子类初始构造钩子函数数组，用于存放L.Class.addInitHook增加的初始化绑定事件钩子函数
         proto._initHooks = [];
 
         var parent = this;
+
         NewClass.__super__ = parent.prototype;
-        
+        //调用父类及其父类等的初始化绑定事件钩子函数
         proto.callInitHooks = function() {
 
             if (this._initHooksCalled) {return};
